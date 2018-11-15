4b9f2ea5804ce84c560b5ae830f6fd3163680e07
ritapaizs <42063568+ritapaizs@users.noreply.github.com>
2018-11-15 12:15:26 +0200

    lab7

--- lab7-4.js
+++ lab7-4.js
@@ -1,23 +1,24 @@
+
 class Animal{
 
-	constructor(color,type)
+	
+	constructor(color)
 	{
-		public var _type = type;
-		private var _color = color;
-		public getColor();
-		//this.getColor = function(){return _color;}
+		 var _type = type;
+		 var _color = color;
+		
 	}
 	getColor()
 	{
-    return '${this._color}';;
+    return '${this._color}';
 	}
 }
 class Fish extends Animal
 {
 	constructor(fishType)
 	{
-		private var _fishType = fishType;
-		public getFishType();
+		 var _fishType = fishType;
+		 getFishType();
 		this.getFishType = function(){return _fishType;}
 	}
 
@@ -25,26 +26,81 @@ class Fish extends Animal
 
 class Dog extends Animal
 {
-constructor(dogType)
+
+	constructor(dt)
 	{
-		private var _dogType = dogType;
-	//	private let dogType = new dogType('name','red');
-		public getdogType();
-		this.getdogType = function(){return _dogType;}
+		let dt = new dogType();
+		var _dt = dt;
+	}
+	getFurColor()
+	{
+    return '${this._dt}';;
 	}
 }
 class dogType
 {
-	constructor(typeName,furColor)
+	constructor(furColor)
 	{
-		public var _typeName = typeName;
-		private var _furColor = furColor;
+		 var _typeName = typeName;
+		 var _furColor = furColor;
 		
 	}
 	getFurColor()
 	{
-    return '${this._furColor}';;
+		console.log('red');
+    return '${this._furColor}';
 	}
 }
-let doggy = new Dog('teckel');
-doggy.getColor();
+
+
+
+//es5
+var type;
+function Animal1(color)
+{
+	this.color = color;
+	this.type = type;
+}
+	function getColor1()
+	{
+    return this.color;
+	}
+var animal = new Animal1('red');
+animal.color;
+animal.type;
+
+
+function Fish1(fishType)
+{
+	this.fishType = fishType;
+}
+function getFishType1()
+	{
+    return this.fishType;
+	}
+
+Fish1.prototype = new Animal1();
+var f = new Fish1();
+console.log(f.color);
+
+
+function Dog1(dogType)
+{
+	this.dogType = dogType;
+	
+}
+	function getFurColor1()
+	{
+    return this.dogType;
+	}
+
+function DogType1(furColor)
+{
+	this.typeName = typeName;
+	this.furColor = furColor;
+}
+	function getFurColor1()
+	{
+    return this.furColor;
+	}
+
